89e8e96dfee4b317c274d72363b54993fc17cf65
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-06 13:40:21 +0200

    (C) del all casques grecs n'ayant jamais été pris

--- requetes.sql
+++ requetes.sql
@@ -138,7 +138,15 @@ INSERT INTO autoriser_boire (id_potion, id_personnage)
 VALUES ('1', '12')
 
 -- C. Supprimez les casques grecs qui n'ont jamais été pris lors d'une bataille.
-
+DELETE FROM casque
+WHERE id_type_casque IN (
+  SELECT id_type_casque
+  FROM type_casque
+  WHERE nom_type_casque = 'Grec'
+) AND id_casque NOT IN (
+  SELECT id_casque
+  FROM prendre_casque
+)
 
 -- D². Modifiez l'adresse de Zérozérosix : il a été mis en prison à Condate.
 
